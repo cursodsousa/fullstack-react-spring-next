0cd56d86da319420a1e4b9aed08c1b3dffa7de7f
Dougllas Sousa <dougllasfps@gmail.com>
2021-04-21 11:00:24 -0300

    formulario de produtos

--- vendas-app/src/components/produtos/cadastro/index.tsx
+++ vendas-app/src/components/produtos/cadastro/index.tsx
@@ -3,7 +3,52 @@ import { Layout } from 'components'
 export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo="Produtos">
-           Aqui vai ficar o meu formulário 
+           <div className="field">
+                <label className="label" htmlFor="inputSku">SKU: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputSku"
+                           placeholder="Digite o SKU do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputPreco">Preço: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputPreco"
+                           placeholder="Digite o Preço do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputNome">Nome: *</label>
+                <div className="control">
+                    <input className="input" 
+                           id="inputNome"
+                           placeholder="Digite o Nome do produto" />
+                </div>
+           </div>
+
+           <div className="field">
+                <label className="label" htmlFor="inputDesc">Descrição: *</label>
+                <div className="control">
+                    <textarea className="textarea" 
+                           id="inputDesc"
+                           placeholder="Digite a Descrição detalhada do produto" />
+                </div>
+           </div>
+
+           <div className="field is-grouped">
+                <div className="control is-link">
+                    <button className="button">Salvar</button>
+                </div>
+                <div className="control">
+                    <button className="button">Voltar</button>
+                </div>
+           </div>
+
         </Layout>
     )
 }
+
