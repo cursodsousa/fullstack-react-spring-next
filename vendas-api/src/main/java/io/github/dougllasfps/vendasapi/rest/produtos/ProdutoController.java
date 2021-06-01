7e8a96a5c0d9115106e57c861a002c0fedef7dad
Dougllas Sousa <dougllasfps@gmail.com>
2021-06-01 08:36:20 -0300

    produtocontroller

--- vendas-api/src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoController.java
+++ vendas-api/src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoController.java
@@ -1,11 +1,14 @@
 package io.github.dougllasfps.vendasapi.rest.produtos;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +27,13 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository repository;
+	
+	@GetMapping
+	public List<ProdutoFormRequest> getLista(){
+		return repository.findAll().stream()
+				.map( ProdutoFormRequest::fromModel )
+				.collect(Collectors.toList());
+	}
 
 	@PostMapping
 	public ProdutoFormRequest salvar( @RequestBody ProdutoFormRequest produto ) {
