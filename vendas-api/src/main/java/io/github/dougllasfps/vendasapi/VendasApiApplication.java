a49b174a0cba0633beb085b7cfa65f9c119b58a6
Dougllas Sousa <dougllasfps@gmail.com>
2021-07-27 16:27:52 -0300

    toma

--- vendas-api/src/main/java/io/github/dougllasfps/vendasapi/VendasApiApplication.java
+++ vendas-api/src/main/java/io/github/dougllasfps/vendasapi/VendasApiApplication.java
@@ -1,10 +1,25 @@
 package io.github.dougllasfps.vendasapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApiApplication {
+	
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/env")
+	public String getProperties() {
+		String ambiente = env.getActiveProfiles()[0];
+		String appName = env.getProperty("spring.application.name");
+		return String.format("Ambiente: %s | Nome da App: %s", ambiente, appName);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
